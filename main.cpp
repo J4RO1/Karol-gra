4a69bf50f57c673436034a3b27a2cf4e981aee31
Karol Janicki <janickikarol4@gmail.com>
2026-08-28 00:26:12 +0200

    Wstep i pierwsze menu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
-#include <windows.h>
 #include <conio.h>
 using namespace std;
-int main() {
-    char c=_getch();
-    cout<<c;
+int main()
+{
+cout<<"Witaj drogi graczu w grze o wezu Jimie!\nJim jest bardzo glodny, wiec pomoz mu znalezc i zjesc wszystkie jablka!\n\nWcisnij dowolny przycisk.";
+_getch();
+int opcje[]={10,15,20,25};
+int wybrana=0;
+int liczba_opcji=4;
+while(true){
+    system("cls");
+    cout<<"No to zaczynamy! Najpierw, jakie wymiary ma miec kwadratowa plansza dla Jima?\n(pssst, wybieraj strzalkami i nacisnij enter)\n\n";
+    for(int i=0;i<liczba_opcji;i++){
+        if(i==wybrana) cout<<"> ";
+        else cout<<"  ";
+        cout<<opcje[i]<<endl;
+    }
+    int klawisz=_getch();
+    if(klawisz==224){
+        klawisz=_getch();
+        if(klawisz==72) wybrana=(wybrana+liczba_opcji-1)%liczba_opcji;
+        if(klawisz==80) wybrana=(wybrana+1)%liczba_opcji;
+    }
+    if(klawisz==13) break;
+}
+system("cls");
+int wymiar=opcje[wybrana];
+cout<<"Wybrano: "<<wymiar;
 }
--- proby.cpp
+++ proby.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
-#include <windows.h>
 #include <conio.h>
 using namespace std;
 int main(){
-string opcje[]={"10","20","30"};
+string opcje[]={"Nowa gra","Wczytaj gre","Ustawienia","Wyjscie"};
 int wybrana=0;
-int liczba_opcji=3;
+int liczba_opcji=4;
 while(true){
     system("cls");
-    cout<<"Jakie wymiary ma miec plansza?"<<endl<<endl;
+    cout<<"===================="<<endl;
+    cout<<"       WYBIERZ"<<endl;
+    cout<<"====================\n"<<endl;
     for(int i=0;i<liczba_opcji;i++){
-        if(i==wybrana) cout<<"> "<<opcje[i]<<endl;
-        else cout<<"  "<<opcje[i]<<endl;
+        if(i==wybrana) cout<<"> ";
+        else cout<<"  ";
+        cout<<opcje[i]<<endl;
     }
     int klawisz=_getch();
     if(klawisz==224){
         klawisz=_getch();
-        if(klawisz==72) wybrana=(wybrana+2)%3;
-        if(klawisz==80) wybrana=(wybrana+1)%3;
+        if(klawisz==72) wybrana=(wybrana+liczba_opcji-1)%liczba_opcji;
+        if(klawisz==80) wybrana=(wybrana+1)%liczba_opcji;
     }
-    else if(klawisz==13) break;
+    if(klawisz==13) break;
 }
 system("cls");
-cout<<"Wybrales: "<<opcje[wybrana]<<endl;
-
+cout<<"wybrano: "<<opcje[wybrana];
 }
